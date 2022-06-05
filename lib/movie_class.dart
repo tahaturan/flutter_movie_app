d511aa19b1ed71406baf4c8968243f0c57715624
Taha Turan <tahaaturann16@gmail.com>
2022-06-06 02:27:47 +0300

    Film Uygulamasi - ilk hazirliklar yapildi

---
+++ lib/movie_class.dart
@@ -0,0 +1,12 @@
+class Movie {
+  int movieId;
+  String movieName;
+  double moviePrice;
+  String movieImage;
+
+  Movie(
+      {required this.movieId,
+      required this.movieName,
+      required this.moviePrice,
+      required this.movieImage});
+}
